100f1f92f2890b82413a7b609d32c3705633a746
Christian Ballejo <christianballejo@gmail.com>
2021-02-18 15:14:49 -0300

    Inicio Intro2

---
+++ tidycovid19_alumnos.R
@@ -0,0 +1,51 @@
+## para instalar paquete tidycovid19
+
+library(devtools)
+
+install_github("joachim-gassen/tidycovid19")
+
+## activamos paquetes
+
+library(tidyverse)
+library(tidycovid19)
+
+## leemos datos
+datos <- download_merged_data(silent = T, cached = T)
+
+## estructura de la tabla
+glimpse(datos)
+
+## visualizamos datos de Argentina
+datos %>%  filter(country  == "Argentina") %>% View()
+
+## gráfico de incidencia acumulada 
+datos %>% 
+  arrange(date) %>% 
+  filter(iso3c %in% c("ARG", "USA")) %>% 
+  mutate(inc = confirmed/population*100000) %>% 
+  ggplot(aes(x = date, y = inc, color = country)) +
+  geom_line(size = 1) +
+  scale_color_manual(values = c("blue", "red"), name = "País")  +
+  xlab("Fecha") + 
+  ylab("Incidencia acumulada (x 100.000 hab.") +
+  labs(title = "Incidencia acumulada Covid-19. Comparativo Argentina - USA (2020-2021)",
+       caption = "Fuente: Dataset del Johns Hopkins University Center for Systems Science and Engineering") 
+
+## comparamos Argentina y Gran Bretaña
+
+
+
+
+## cambiamos color azul por verde (green)
+
+
+
+
+## incorporamos otro país - España (ESP)
+
+
+
+
+
+
+## grafico de mortalidad
